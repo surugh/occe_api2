18c9fb959f7071f9575aab61f2f74b0e9c22ba96
surugh <35753081+surugh@users.noreply.github.com>
2021-05-17 11:07:42 +0300

    Add files via upload
    fixed upper() to lower()

--- occe_api_v2.py
+++ occe_api_v2.py
@@ -169,7 +169,7 @@ class Occe:
         balances = self.get_balances()['data']['currencies']
         balance = []
         for info in balances:
-            if info['name'] == coin.upper():
+            if info['name'] == coin.lower():
                 balance.append(info['value'])
         return balance[0]
 
@@ -212,7 +212,7 @@ class Occe:
         :return: Активные заказы пользователя | Active user orders
         :type: dict
         """
-        req = self.call_api('account/orders/open/' + pair.upper())
+        req = self.call_api('account/orders/open/' + pair.lower())
         return req
 
     def get_orders_history(self, pair):
@@ -240,7 +240,7 @@ class Occe:
         :return: Историю заказов пользователя | User's order history
         :type: dict
         """
-        req = self.call_api('account/orders_history/' + pair.upper())
+        req = self.call_api('account/orders_history/' + pair.lower())
         return req
 
     def get_orders_status(self, pair):
@@ -291,7 +291,7 @@ class Occe:
         :return:  {"result": "success", "info": "Order was deleted"}
         :type: dict
         """
-        req = self.call_api(pair.upper() + '/orders' + str(order_id), get=False, delete=True)
+        req = self.call_api(pair.lower() + '/orders' + str(order_id), get=False, delete=True)
         return req
 
     def create_order(self, market, order_type, amount, price):
@@ -317,12 +317,12 @@ class Occe:
         :type: dict
         """
         balance_version = str(self.get_balances()['data']['balanceVersion'])
-        data = dict(type=order_type.upper(),
+        data = dict(type=order_type.lower(),
                     amount=amount,
                     price=price,
                     balanceVersion=balance_version)
 
-        req = self.call_api(market.upper() + '/orders', params=data, get=False)
+        req = self.call_api(market.lower() + '/orders', params=data, get=False)
         return req
 
     def get_server_time(self):
@@ -378,7 +378,7 @@ class Occe:
         :type: dict
         """
         if pair:
-            req = requests.get(self.api_url_public + 'info/' + pair.upper())
+            req = requests.get(self.api_url_public + 'info/' + pair.lower())
         else:
             req = requests.get(self.api_url_public + 'info/')
         return req.json()
@@ -416,7 +416,7 @@ class Occe:
         :return: Все активные ордера по выбранной паре | All active orders for the selected pair
         :type: dict
         """
-        req = requests.get(self.api_url_public + 'orders/' + pair.upper())
+        req = requests.get(self.api_url_public + 'orders/' + pair.lower())
         return req.json()
 
 
@@ -427,8 +427,8 @@ if __name__ == '__main__':
     occe = Occe(access_key=PRIVATE['access'], secret_key=PRIVATE['secret'])
 
     try:
-        print(type(occe.get_server_time()))
-        # print(occe.get_trade_history('idna_uah'))
+        print(occe.get_server_time())
+        print(occe.get_trade_history('IDNA_UAH'))
         # print(occe.get_market_orders('idna_usdt'))
         # print(occe.get_balances())
         # print(occe.get_balance('RUB'))
